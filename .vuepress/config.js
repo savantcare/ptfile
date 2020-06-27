aaf63a0b68daf09b11b1d964fddc26c37d41af8c
Vikas <kedia.vikas@gmail.com>
2020-06-27 14:22:42 -0700

    bc

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,6 +3,19 @@ module.exports = {
   description: "Big picture, server side and client side doc",
   patterns: ["!**/**/node_modules", "**/*.md", "**/*.vue"],
   themeConfig: {
-    sidebar: ["/", "/docs/"],
+    sidebar: [
+      {
+        title: "Home", // required
+        path: "/", // optional, link of the title, which should be an absolute path and must exist
+        collapsable: true, // optional, defaults to true
+        sidebarDepth: 1, // optional, defaults to 1
+      },
+      {
+        title: "Big picture", // required
+        path: "/docs/", // optional, link of the title, which should be an absolute path and must exist
+        collapsable: true, // optional, defaults to true
+        sidebarDepth: 1, // optional, defaults to 1
+      },
+    ],
   },
 };
