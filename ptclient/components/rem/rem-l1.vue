e745d4863a47d9f81912fdc95527e895067b5bc2
Vikas <kedia.vikas@gmail.com>
2020-06-28 05:07:48 -0700

    bc

--- ptclient/components/rem/rem-l1.vue
+++ ptclient/components/rem/rem-l1.vue
@@ -4,15 +4,20 @@
       <div slot="header" class="clearfix">
         <span>Reminder</span>
         <el-button style="float: right; padding: 3px 0;" type="text"
+          >X</el-button
+        >
+        <el-button style="float: right; padding: 3px 0;" type="text"
+          >D</el-button
+        >
+        <el-button style="float: right; padding: 3px 0;" type="text"
           >M</el-button
         >
       </div>
       <el-table :data="tableData" style="width: 100%;">
-        <el-table-column prop="date" label="Date" width="180">
+        <el-table-column prop="description" label="Description" width="180">
         </el-table-column>
-        <el-table-column prop="name" label="Name" width="180">
+        <el-table-column prop="createdAt" label="Created At" width="180">
         </el-table-column>
-        <el-table-column prop="address" label="Address"> </el-table-column>
       </el-table>
     </el-card>
   </div>
@@ -24,24 +29,8 @@ export default {
     return {
       tableData: [
         {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
+          createdAt: '2016-05-03',
+          description: 'TSH test in 3 months',
         },
       ],
     }
