4d9a51b82084afed426288ea53047f3c27d3a302
Vikas <kedia.vikas@gmail.com>
2020-06-27 18:07:43 -0700

    bc

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,13 @@
 module.exports = {
   title: "Patient file",
   description: "Big picture, server side and client side doc",
-  patterns: ["!**/**/node_modules", "**/*.md", "**/*.vue"],
+
+  /* Two consecutive asterisks ** in patterns matched against full pathname may have special meaning:
+      A leading ** followed by a slash means match in all directories. For example, **slashfoo matches file or directory foo anywhere, the same as pattern foo. **slashfoo/bar matches file or directory bar anywhere that is directly under directory foo.
+    A trailing /** matches everything inside. For example, abc/** matches all files inside directory abc, relative to the location of the .gitignore file, with infinite depth.
+    Ref: https://stackoverflow.com/questions/29820791/git-ignore-node-modules-folder-everywhere
+  */
+  patterns: ["!(**/node_modules/**)", "**/*.md", "**/*.vue"],
   themeConfig: {
     sidebar: [
       {
