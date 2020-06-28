6ad52d82f2c08d27963fa48f5567f3d295c8f6a3
Vikas <kedia.vikas@gmail.com>
2020-06-28 04:55:28 -0700

    bc

---
+++ ptclient/components/rex/rex-l1.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>Recommendations</span>
+        <el-button style="float: right; padding: 3px 0;" type="text"
+          >M</el-button
+        >
+      </div>
+      <el-table :data="tableData" style="width: 100%;">
+        <el-table-column prop="date" label="Date" width="180">
+        </el-table-column>
+        <el-table-column prop="name" label="Name" width="180">
+        </el-table-column>
+        <el-table-column prop="address" label="Address"> </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+      ],
+    }
+  },
+}
+</script>
--- ptclient/pages/pf/index.vue
+++ ptclient/pages/pf/index.vue
@@ -2,10 +2,11 @@
   <div>
     <Split style="height: 900px; width: 1400px;" :gutterSize="5">
       <SplitArea :size="75">
-        panel left
+        <ctRexL1> </ctRexL1>
+        <ctRemL1> </ctRemL1>
       </SplitArea>
       <SplitArea :size="25">
-        panel right
+        <ctRexL1> </ctRexL1>
         <ctSearchBox></ctSearchBox>
       </SplitArea>
     </Split>
@@ -16,11 +17,15 @@
 import Vue from 'vue'
 import VueSplit from 'vue-split-panel'
 import ctSearchBox from '@/components/search-box'
+import ctRexL1 from '@/components/rex/rex-l1'
+import ctRemL1 from '@/components/rem/rem-l1'
 
 Vue.use(VueSplit)
 export default {
   components: {
     ctSearchBox,
+    ctRexL1,
+    ctRemL1,
   },
 }
 </script>
