c96cb28f5ffd4159a1c0ad0206ee987295589e82
Vikas <kedia.vikas@gmail.com>
2020-06-28 01:23:40 -0700

    bc

--- ptclient/pages/pf/index.vue
+++ ptclient/pages/pf/index.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
     Jai kali ma
+    <Split style="height: 500px;">
+      <SplitArea :size="25">
+        panel left
+      </SplitArea>
+      <SplitArea :size="75">
+        panel right
+      </SplitArea>
+    </Split>
   </div>
 </template>
+
+<script>
+import VueSplit from 'vue-split-panel'
+Vue.use(VueSplit)
+export default {
+    
+}
+</script>
