267ba83bda2f697e35214fc059e8028a20bf10f7
Vikas <kedia.vikas@gmail.com>
2020-06-27 18:15:50 -0700

    bc

--- start-dev-servers-in-tmux.sh
+++ start-dev-servers-in-tmux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tmux new-session -s projectname 'npm run docs-in-dev-mode' \; split-window 'cd ptclient; yarn dev' \; split-window \; select-layout tiled \;
+tmux new-session -s projectname 'yarn docs-in-dev-mode' \; split-window 'cd ptclient; yarn dev' \; split-window \; select-layout tiled \;
