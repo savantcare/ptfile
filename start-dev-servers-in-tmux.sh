c5ce04d2d5e01e56d315d79b77e684f94d5ba6a9
Vikas <kedia.vikas@gmail.com>
2020-06-28 21:33:37 -0700

    bc

--- start-dev-servers-in-tmux.sh
+++ start-dev-servers-in-tmux.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-tmux new-session -s projectname 'yarn docs-in-dev-mode' \; split-window 'cd ptclient; yarn dev' \; split-window \; select-layout tiled \;
+
+# Here panel exit when program ends
+# tmux new-session -s projectname 'yarn docs-in-dev-mode' \; split-window 'cd ptclient; yarn dev' \; split-window \; select-layout tiled \;
+
+# Here panes do not exit when command ends
+tmuxinator start all-dev-servers-in-tmux
