a3f55a89328bccc38726e8b3ead9e36cdb3176c4
Vikas <kedia.vikas@gmail.com>
2020-06-28 21:54:21 -0700

    bc

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: "Patient file",
-  description: "Big picture, server side and client side doc",
+  title: 'Patient file',
+  description: 'Big picture, server side and client side doc',
 
   evergreen: true, // Only support latest browser Ref: https://vuepress.vuejs.org/config/#evergreen
 
@@ -10,101 +10,101 @@ module.exports = {
     Ref: https://stackoverflow.com/questions/29820791/git-ignore-node-modules-folder-everywhere
   */
   patterns: [
-    "!(**/node_modules/**)",
-    "**/*.md",
-    "**/*.vue",
-    "!.vuepress",
-    "!node_modules",
-    "!.*",
+    '!(**/node_modules/**)',
+    '**/*.md',
+    '**/*.vue',
+    '!.vuepress',
+    '!node_modules',
+    '!.*',
   ],
-  plugins: ["vuepress-plugin-glossary"],
+  plugins: ['vuepress-plugin-glossary'],
   themeConfig: {
     sidebar: [
       {
-        title: "Home", // required
-        path: "/", // optional, link of the title, which should be an absolute path and must exist
+        title: 'Home', // required
+        path: '/', // optional, link of the title, which should be an absolute path and must exist
         collapsable: true, // optional, defaults to true
         sidebarDepth: 1, // optional, defaults to 1
         children: [
           {
-            path: "/faq",
-            title: "FAQ",
+            path: '/faq',
+            title: 'FAQ',
           },
         ],
       },
       {
-        title: "Big picture",
-        path: "/big-picture/",
+        title: 'Big picture',
+        path: '/big-picture/',
         collapsable: true,
         sidebarDepth: 2,
         // How to do nested sidebar? https://github.com/vuejs/vuepress/issues/287#issuecomment-480805333
         children: [
           {
-            path: "/big-picture/ui/",
-            title: "UI",
+            path: '/big-picture/ui/',
+            title: 'UI',
             children: [
               {
-                path: "/big-picture/ui/why",
-                title: "Why",
+                path: '/big-picture/ui/why',
+                title: 'Why',
               },
               {
-                path: "/big-picture/ui/what",
-                title: "What",
+                path: '/big-picture/ui/what',
+                title: 'What',
               },
             ],
           },
           {
-            path: "/big-picture/tech/",
-            title: "Tech",
+            path: '/big-picture/tech/',
+            title: 'Tech',
             children: [
               {
-                path: "/big-picture/tech/why",
-                title: "Why",
+                path: '/big-picture/tech/why',
+                title: 'Why',
               },
               {
-                path: "/big-picture/tech/what",
-                title: "What",
+                path: '/big-picture/tech/what',
+                title: 'What',
               },
               {
-                path: "/big-picture/tech/how",
-                title: "How",
+                path: '/big-picture/tech/how',
+                title: 'How',
               },
             ],
           },
         ],
       },
       {
-        title: "Server",
-        path: "/ptserver/",
+        title: 'Server',
+        path: '/ptserver/',
         collapsable: true,
         sidebarDepth: 1,
       },
       {
-        title: "Client",
-        path: "/ptclient/",
+        title: 'Client',
+        path: '/ptclient/',
         collapsable: true,
         sidebarDepth: 1,
         children: [
           {
-            path: "/ptclient/components/",
-            title: "Components",
+            path: '/ptclient/components/',
+            title: 'Components',
             children: [
               {
-                path: "/ptclient/components/recommendations/",
-                title: "Recommendations",
+                path: '/ptclient/components/rex/',
+                title: 'Recommendations',
               },
               {
-                path: "/ptclient/components/psych-review-of-systems/",
-                title: "Psych review of systems",
+                path: '/ptclient/components/psych-review-of-systems/',
+                title: 'Psych review of systems',
               },
             ],
           },
         ],
       },
       {
-        title: "Glossary",
-        path: "/GLOSSARY",
+        title: 'Glossary',
+        path: '/GLOSSARY',
       },
     ],
   },
-};
+}
