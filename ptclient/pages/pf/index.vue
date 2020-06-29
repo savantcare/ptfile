35f473d7ccf23207b6c3036bc86387bf0148d247
Vikas <kedia.vikas@gmail.com>
2020-06-29 02:02:27 -0700

    bc

--- ptclient/pages/pf/index.vue
+++ ptclient/pages/pf/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    Choose a patient TP1: <br />
+    Choose a patient <br />
     <br />
     <a href="/pf/abcd">TP1: Test patient 1</a>
     <br /><br />
-    <a href="/pf/efgh">TP2: Test patient 1</a>
+    <a href="/pf/efgh">TP2: Test patient 2</a>
   </div>
 </template>
 
