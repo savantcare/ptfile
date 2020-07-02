4bc6b356595efddbd166fea58651beab79f4ebee
Vikas <kedia.vikas@gmail.com>
2020-07-01 19:03:32 -0700

    bc

--- ptclient/components/ctMaster/db-structure.sql
+++ ptclient/components/ctMaster/db-structure.sql
@@ -1,23 +1,13 @@
-CREATE TABLE `ctMaster`
-(
-  `uuid` char
-(36) NOT NULL,
-  `name` varchar
-(255) DEFAULT NULL,
-  `tag` varchar
-(255) DEFAULT NULL,
-  `recordChangedByUUID` char
-(36) NOT NULL,
-  `recordChangedFromIPAddress` varchar
-(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
-WITH SYSTEM VERSIONING;;
+CREATE TABLE `ctMaster` (
+  `uuid` char(36) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `tag` varchar(255) DEFAULT NULL,
+  `recordChangedByUUID` char(36) NOT NULL,
+  `recordChangedFromIPAddress` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 WITH SYSTEM VERSIONING;;
 
 
-INSERT INTO `ctMaster` (`
-uuid`,
-`tag
-`, `name`, `recordChangedByUUID`, `recordChangedFromIPAddress`) VALUES
+INSERT INTO `ctMaster` (`uuid`, `tag`, `name`, `recordChangedByUUID`, `recordChangedFromIPAddress`) VALUES
 ('1', 'health', 'Recommendation', '1', '202.005.122.057'),
 ('3', 'health', 'Reminder', '3', '202.005.122.057'),
 ('4', 'health', 'BM', '3', '202.005.122.057'),
