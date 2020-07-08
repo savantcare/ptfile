7d7fd2634334f6edeab3597ca6b8f746e3c1950f
Vikas <kedia.vikas@gmail.com>
2020-07-08 06:52:37 -0700

    on resetForm the input fields go away

--- ptclient/components/rem/cl/add.vue
+++ ptclient/components/rem/cl/add.vue
@@ -150,6 +150,7 @@ export default {
       } else{
         console.log('No Unsaved data')
       }
+      this.arReminderID = []
     },
     removedescription(item) {
       var index = this.dynamicValidateForm.descriptions.indexOf(item);
