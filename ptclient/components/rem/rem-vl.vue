d2cdb34dfcda175625a01ec22fd466b506177915
Vikas <kedia.vikas@gmail.com>
2020-07-07 23:57:58 -0700

    dtop multiple reminders coming in search

--- ptclient/components/rem/rem-vl.vue
+++ ptclient/components/rem/rem-vl.vue
@@ -38,20 +38,27 @@ export default {
     }
   },
   mounted() {
-    ormCTLifeCycle.insert({
-      data: {
-        name: 'Reminder',
-        status: 3,
-      },
-    })
-    ormSearchUiToCT.insert({
-      data: {
-        value: 'Reminders',
-        ctAbbr: 'rem',
-        ctToShowInsideTab: 'rem/rem-vl.vue',
-        layer: 'view',
-      },
-    })
+    // Why do I check for lifecycle before defining search terms. Reason given at: rex-l1.vue:49
+    const resultSet = ormCTLifeCycle.query().where('name', 'Reminder').get()
+    const resultData = resultSet[0]
+    if (typeof resultData !== 'undefined') {
+      console.log('already mounted')
+    } else {
+      ormCTLifeCycle.insert({
+        data: {
+          name: 'Reminder',
+          status: 3,
+        },
+      })
+      ormSearchUiToCT.insert({
+        data: {
+          value: 'Reminders',
+          ctAbbr: 'rem',
+          ctToShowInsideTab: 'rem/rem-vl.vue',
+          layer: 'view',
+        },
+      })
+    }
   },
 }
 </script>
