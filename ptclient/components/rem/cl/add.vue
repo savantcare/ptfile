c5e6bc88fee3166b57cb8d2a416c5116d7cc3d19
Vikas <kedia.vikas@gmail.com>
2020-07-08 00:11:33 -0700

    reminder has multi add

--- ptclient/components/rem/cl/add.vue
+++ ptclient/components/rem/cl/add.vue
@@ -1,27 +1,90 @@
 <template>
-  <div>
-    <el-card class="box-card">
-      <div slot="header" class="clearfix">
-        <span>Reminder</span>
-        <el-button style="float: right; padding: 3px 0;" type="text"
-          >X</el-button
-        >
-        <el-button style="float: right; padding: 3px 0;" type="text"
-          >D</el-button
-        >
-        <el-button style="float: right; padding: 3px 0;" type="text"
-          >M</el-button
-        >
-      </div>
-      <el-table :data="daTable" style="width: 100%;">
-        <el-table-column prop="description" label="Description" width="180">
-        </el-table-column>
-        <el-table-column prop="createdAt" label="Created At" width="180">
-        </el-table-column>
-      </el-table>
-    </el-card>
-  </div>
+  <el-form
+    :model="dynamicValidateForm"
+    ref="dynamicValidateForm"
+    label-width="120px"
+    class="demo-dynamic"
+  >
+    <el-form-item
+      prop="email"
+      label="Email"
+      :rules="[
+        {
+          required: true,
+          message: 'Please input email address',
+          trigger: 'blur',
+        },
+        {
+          type: 'email',
+          message: 'Please input correct email address',
+          trigger: ['blur', 'change'],
+        },
+      ]"
+    >
+      <el-input v-model="dynamicValidateForm.email"></el-input>
+    </el-form-item>
+    <el-form-item
+      v-for="(domain, index) in dynamicValidateForm.domains"
+      :label="'Domain' + index"
+      :key="domain.key"
+      :prop="'domains.' + index + '.value'"
+      :rules="{
+        required: true,
+        message: 'domain can not be null',
+        trigger: 'blur',
+      }"
+    >
+      <el-input v-model="domain.value"></el-input
+      ><el-button @click.prevent="removeDomain(domain)">Delete</el-button>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="submitForm('dynamicValidateForm')"
+        >Submit</el-button
+      >
+      <el-button @click="addDomain">New domain</el-button>
+      <el-button @click="resetForm('dynamicValidateForm')">Reset</el-button>
+    </el-form-item>
+  </el-form>
 </template>
-
 <script>
+  export default {
+    data() {
+      return {
+        dynamicValidateForm: {
+          domains: [{
+            key: 1,
+            value: ''
+          }],
+          email: ''
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
+      removeDomain(item) {
+        var index = this.dynamicValidateForm.domains.indexOf(item);
+        if (index !== -1) {
+          this.dynamicValidateForm.domains.splice(index, 1);
+        }
+      },
+      addDomain() {
+        this.dynamicValidateForm.domains.push({
+          key: Date.now(),
+          value: ''
+        });
+      }
+    }
+  }
 </script>
