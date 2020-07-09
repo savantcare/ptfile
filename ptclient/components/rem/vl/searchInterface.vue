cc4a59426f6483e354748988272c292cf6cf0627
Vikas <kedia.vikas@gmail.com>
2020-07-08 21:39:48 -0700

    removed unused code

--- ptclient/components/rem/vl/searchInterface.vue
+++ ptclient/components/rem/vl/searchInterface.vue
@@ -4,7 +4,6 @@
 
 <script>
 import ormSearchUiToCT from '@/components/vl-search-box/vuex-orm/searchUiToCT'
-import ormCTLifeCycle from '@/models/ormCTLifeCycle'
 
 export default {
   data() {
@@ -13,49 +12,36 @@ export default {
     }
   },
   mounted() {
-    // Why do I check for lifecycle before defining search terms. Reason given at: rex-l1.vue:49
-    const resultSet = ormCTLifeCycle.query().where('name', 'Reminder').get()
-    const resultData = resultSet[0]
-    if (typeof resultData !== 'undefined') {
-      console.log('already mounted')
-    } else {
-      ormCTLifeCycle.insert({
-        data: {
-          name: 'Reminder',
-          status: 3,
-        },
-      })
-      ormSearchUiToCT.insert({
-        data: {
-          value: 'Reminders',
-          ctAbbr: 'rem',
-          ctToShowInsideTab: 'rem/cl/add.vue',
-          ctToShowInsideMSVL: 'rem/vl/table.vue',
-          ctToShowInsideCSVL: 'rem/vl/timeline.vue',
-          layer: 'view',
-        },
-      })
+    ormSearchUiToCT.insert({
+      data: {
+        value: 'Reminders',
+        ctAbbr: 'rem',
+        ctToShowInsideTab: 'rem/cl/add.vue',
+        ctToShowInsideMSVL: 'rem/vl/table.vue',
+        ctToShowInsideCSVL: 'rem/vl/timeline.vue',
+        layer: 'view',
+      },
+    })
 
-      ormSearchUiToCT.insert({
-        data: {
-          value: 'Add Reminder',
-          ctAbbr: 'arem',
-          ctToShowInsideTab: 'rem/cl/add.vue',
-          ctToShowInsideMSVL: 'rem/vl/table.vue',
-          ctToShowInsideCSVL: 'rem/vl/timeline.vue',
-          layer: 'change',
-        },
-      })
+    ormSearchUiToCT.insert({
+      data: {
+        value: 'Add Reminder',
+        ctAbbr: 'arem',
+        ctToShowInsideTab: 'rem/cl/add.vue',
+        ctToShowInsideMSVL: 'rem/vl/table.vue',
+        ctToShowInsideCSVL: 'rem/vl/timeline.vue',
+        layer: 'change',
+      },
+    })
 
-      ormSearchUiToCT.insert({
-        data: {
-          value: 'Multi change Reminder',
-          ctAbbr: 'arem',
-          ctToShowInsideTab: 'rem/cl/mc.vue',
-          layer: 'change',
-        },
-      })
-    }
+    ormSearchUiToCT.insert({
+      data: {
+        value: 'Multi change Reminder',
+        ctAbbr: 'arem',
+        ctToShowInsideTab: 'rem/cl/mc.vue',
+        layer: 'change',
+      },
+    })
   },
 }
 </script>
