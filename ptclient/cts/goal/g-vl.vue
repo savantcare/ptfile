56001067f9e5181fa6ea2266b34d5b57bb4aca08
Vikas <kedia.vikas@gmail.com>
2020-07-09 14:31:28 -0700

    g has its own serchInterfaces

--- ptclient/cts/goal/g-vl.vue
+++ ptclient/cts/goal/g-vl.vue
@@ -35,34 +35,6 @@ export default {
     }
   },
   mounted() {
-    // Search interfaces to this component
-    ormSearchUiToCT.insert({
-      data: {
-        value: 'Add goal',
-        ctAbbr: 'addg',
-        ctToShowInCL: 'goal/cl/ctAddGoal.vue',
-        layer: 'change',
-      },
-    })
-
-    ormSearchUiToCT.insert({
-      data: {
-        value: 'Multi rate goal',
-        ctAbbr: 'mcg',
-        ctToShowInCL: 'goal/cl/ctMultiRateGoal.vue',
-        layer: 'change',
-      },
-    })
-
-    ormSearchUiToCT.insert({
-      data: {
-        value: 'Goal',
-        ctAbbr: 'g',
-        ctToShowInCL: 'goal/g-vl.vue',
-        layer: 'view',
-      },
-    })
-
     this.mfdaGetG()
   },
   methods: {
---
+++ ptclient/cts/goal/searchInterfaces.vue
@@ -0,0 +1,44 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import ormSearchUiToCT from '@/cts/vl-search-box/vuex-orm/searchUIToCT'
+
+export default {
+  data() {
+    return {
+      // dataTable: [],
+    }
+  },
+  mounted() {
+    // Search interfaces to this component
+    ormSearchUiToCT.insert({
+      data: {
+        value: 'Add goal',
+        ctAbbr: 'addg',
+        ctToShowInCL: 'goal/cl/ctAddGoal.vue',
+        layer: 'change',
+      },
+    })
+
+    ormSearchUiToCT.insert({
+      data: {
+        value: 'Multi rate goal',
+        ctAbbr: 'mcg',
+        ctToShowInCL: 'goal/cl/ctMultiRateGoal.vue',
+        layer: 'change',
+      },
+    })
+
+    ormSearchUiToCT.insert({
+      data: {
+        value: 'Goal',
+        ctAbbr: 'g',
+        ctToShowInCL: 'goal/g-vl.vue',
+        layer: 'view',
+      },
+    })
+  },
+}
+</script>
