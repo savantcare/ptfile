16480b9ddfac8fbe7c0a67030d2a41a76c9c3d01
Vikas <kedia.vikas@gmail.com>
2020-07-08 23:09:03 -0700

    fixed table

--- ptclient/components/rem/db/structure/sc_reminder/structure-gen-on-2020-06-26.sql
+++ ptclient/components/rem/db/structure/sc_reminder/structure-gen-on-2020-06-26.sql
@@ -1,31 +1,16 @@
 use sc_reminder;
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: mariadb
--- Generation Time: Jun 26, 2020 at 10:46 AM
--- Server version: 10.4.13-MariaDB-1:10.4.13+maria~bionic
--- PHP Version: 7.4.5
---
--- Database: `sc_reminder`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reminder_news`
---
 
 DROP TABLE IF EXISTS `reminder_news`;
 
-CREATE TABLE `reminder_news` (
-  `uuid` char(36) NOT NULL,
-  `patientUUID` char(36) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `recordChangedByUUID` char(36) DEFAULT NULL,
-  `recordChangedFromIPAddress` varchar(255) DEFAULT NULL,
-  `recordChangedFromSection` varchar(255) DEFAULT 'patientFile',
+CREATE TABLE `reminders` (
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `uuidOfRemMadeFor` char(36) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `remDescription` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `notes` text DEFAULT NULL,
+  `priority` int(11) DEFAULT NULL,
+  `isAutoRem` int(11) DEFAULT NULL,
+  `recordChangedByUUID` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `recordChangedFromIPAddress` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `recordChangedFromSection` varchar(255) COLLATE utf8_unicode_ci DEFAULT 'patientFile',
   PRIMARY KEY (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 WITH SYSTEM VERSIONING;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 WITH SYSTEM VERSIONING;
