fee55022ed32d48e7247788edc488a1e48cd65f5
Vikas <kedia.vikas@gmail.com>
2020-07-08 23:11:26 -0700

    fixed typo

--- ptclient/components/rem/db/structure/sc_reminder/structure-gen-on-2020-06-26.sql
+++ ptclient/components/rem/db/structure/sc_reminder/structure-gen-on-2020-06-26.sql
@@ -1,6 +1,6 @@
 use sc_reminder;
 
-DROP TABLE IF EXISTS `reminder_news`;
+DROP TABLE IF EXISTS `reminder`;
 
 CREATE TABLE `reminders` (
   `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
@@ -9,8 +9,8 @@ CREATE TABLE `reminders` (
   `notes` text DEFAULT NULL,
   `priority` int(11) DEFAULT NULL,
   `isAutoRem` int(11) DEFAULT NULL,
-  `recordChangedByUUID` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `recordChangedFromIPAddress` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `recordChangedByUUID` char(36) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `recordChangedFromIPAddress` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `recordChangedFromSection` varchar(255) COLLATE utf8_unicode_ci DEFAULT 'patientFile',
   PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 WITH SYSTEM VERSIONING;
