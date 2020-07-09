4af6b72feba634335697493013c6d3549e07a618
Vikas <kedia.vikas@gmail.com>
2020-07-09 14:28:14 -0700

    easier to read

--- ptclient/cts/rec/searchInterfaces.vue
+++ ptclient/cts/rec/searchInterfaces.vue
@@ -2,31 +2,40 @@
 import ormSearchUiToCT from '@/cts/vl-search-box/vuex-orm/searchUIToCT'
 import ormCTLifeCycle from '@/models/ormCtLifeCycle'
 
-ormCTLifeCycle.insert({
-  data: {
-    name: 'Recommendations',
-    status: 3,
+export default {
+  data() {
+    return {
+      // dataTable: [],
+    }
   },
-})
+  mounted() {
+    ormCTLifeCycle.insert({
+      data: {
+        name: 'Recommendations',
+        status: 3,
+      },
+    })
 
-// Step 2/3: Inserting Search interfaces to this component
-ormSearchUiToCT.insert({
-  data: {
-    value: 'Recommendations',
-    ctAbbr: 'rex',
-    ctToShowInCL: '',
-    ctToShowInMSVL: 'rec/vl/table.vue',
-    ctToShowInCSVL: 'rec/vl/timeline.vue',
-    layer: 'view',
-  },
-})
+    // Step 2/3: Inserting Search interfaces to this component
+    ormSearchUiToCT.insert({
+      data: {
+        value: 'Recommendations',
+        ctAbbr: 'rex',
+        ctToShowInCL: '',
+        ctToShowInMSVL: 'rec/vl/table.vue',
+        ctToShowInCSVL: 'rec/vl/timeline.vue',
+        layer: 'view',
+      },
+    })
 
-ormSearchUiToCT.insert({
-  data: {
-    value: 'Add Recommendations',
-    ctAbbr: 'arex',
-    ctToShowInCL: 'rec/vl/table.vue',
-    layer: 'change',
+    ormSearchUiToCT.insert({
+      data: {
+        value: 'Add Recommendations',
+        ctAbbr: 'arex',
+        ctToShowInCL: 'rec/vl/table.vue',
+        layer: 'change',
+      },
+    })
   },
-})
+}
 </script>
