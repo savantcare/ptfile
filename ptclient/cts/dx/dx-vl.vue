67b0e508cb4fec630f84708a6e8777ca96c9c0de
Vikas <kedia.vikas@gmail.com>
2020-07-09 14:33:54 -0700

    dx has its own serchInterfaces

--- ptclient/cts/dx/dx-vl.vue
+++ ptclient/cts/dx/dx-vl.vue
@@ -42,31 +42,6 @@ export default {
     }
   },
   mounted() {
-    // Search interfaces to this component
-    ormSearchUiToCT.insert({
-      data: {
-        value: 'Add diagnosis',
-        ctAbbr: 'adddx',
-        ctToShowInCL: 'dx/cl/ctAddDx.vue',
-        layer: 'change',
-      },
-    })
-    ormSearchUiToCT.insert({
-      data: {
-        value: 'Diagnosis',
-        ctAbbr: 'dx',
-        ctToShowInCL: 'dx/dx-vl.vue',
-        layer: 'view',
-      },
-    })
-    ormSearchUiToCT.insert({
-      data: {
-        value: 'Multichange dx assessment',
-        ctAbbr: 'mcdxa',
-        ctToShowInCL: 'dx/cl/ctMultiChangeDxa.vue',
-        layer: 'change',
-      },
-    })
     this.mfdaGetDx()
     this.mfdaGetDxa()
   },
---
+++ ptclient/cts/dx/searchInterfaces.vue
@@ -0,0 +1,42 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import ormSearchUiToCT from '@/cts/vl-search-box/vuex-orm/searchUIToCT'
+
+export default {
+  data() {
+    return {
+      // dataTable: [],
+    }
+  },
+  mounted() {
+    // Search interfaces to this component
+    ormSearchUiToCT.insert({
+      data: {
+        value: 'Add diagnosis',
+        ctAbbr: 'adddx',
+        ctToShowInCL: 'dx/cl/ctAddDx.vue',
+        layer: 'change',
+      },
+    })
+    ormSearchUiToCT.insert({
+      data: {
+        value: 'Diagnosis',
+        ctAbbr: 'dx',
+        ctToShowInCL: 'dx/dx-vl.vue',
+        layer: 'view',
+      },
+    })
+    ormSearchUiToCT.insert({
+      data: {
+        value: 'Multichange dx assessment',
+        ctAbbr: 'mcdxa',
+        ctToShowInCL: 'dx/cl/ctMultiChangeDxa.vue',
+        layer: 'change',
+      },
+    })
+  },
+}
+</script>
