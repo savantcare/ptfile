ddc8c0caa604664aa4415c762d875d5256e915d6
Vikas <kedia.vikas@gmail.com>
2020-07-10 05:07:25 -0700

    now able to show data with same uuid

--- ptclient/cts/rem/cl/c.vue
+++ ptclient/cts/rem/cl/c.vue
@@ -13,6 +13,18 @@
         >
       </el-form-item>
     </el-form>
+
+    <!-- Goal show history of this reminder -->
+    <el-timeline style="padding-inline-start: 20px;">
+      <el-timeline-item
+        v-for="row in dataTable"
+        :key="row.createdAt"
+        :timestamp="row.createdAt"
+        :type="row.type"
+      >
+        {{ row.remDescription }}
+      </el-timeline-item>
+    </el-timeline>
   </div>
 </template>
 <script>
@@ -23,6 +35,42 @@ export default {
     return {
       doRem: {},
     }
+  },
+  computed: {
+    dataTable() {
+      const dataTable = []
+      const resultSet = ormRem.query().where('uuid', this.firstParam).get()
+      if (resultSet.length) {
+        let obj = []
+        let date = ''
+        console.log('sending this to timeline', resultSet, resultSet[0].uuid)
+        for (let i = 0; i < resultSet.length; i++) {
+          obj = {}
+          obj.remDescription = resultSet[i].remDescription
+          /*
+          To get the number of the month:
+          obj.createdAt = date.getMonth() + 1 + "-" + date.getDate()
+
+          To get the name of the month:
+          Ref: https://stackoverflow.com/questions/1643320/get-month-name-from-date
+          */
+          date = new Date(resultSet[i].ROW_START)
+          obj.createdAt =
+            date.toLocaleString('default', { month: 'long' }) +
+            '-' +
+            date.getDate()
+          if (resultSet[i].$isDirty) {
+            obj.type = 'warning'
+          } else {
+            obj.type = ''
+          }
+
+          dataTable.push(obj)
+        }
+      }
+      console.log(dataTable)
+      return dataTable
+    },
   },
   methods: {
     getDescription() {
