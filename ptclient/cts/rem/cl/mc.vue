29a4b5c80f053331272724123bc818fb79678b33
Vikas <kedia.vikas@gmail.com>
2020-07-10 04:52:39 -0700

    mc is able to use c

--- ptclient/cts/rem/cl/mc.vue
+++ ptclient/cts/rem/cl/mc.vue
@@ -1,56 +1,30 @@
 <template>
-  <el-form label-width="120px">
-    <el-form-item v-for="id in arRemID" label="desc" :key="id">
-      <el-input
-        :value="getDescription(id)"
-        @input="setDescription($event, id)"
-      ></el-input>
-    </el-form-item>
-  </el-form>
+  <el-row type="flex">
+    <div v-for="uuid in daRemUUID" :key="uuid">
+      <c :firstParam="uuid"></c>
+    </div>
+
+    <c></c>
+  </el-row>
 </template>
 <script>
 import ormRem from '@/cts/rem/vuex-orm/model.js'
+import c from '@/cts/rem/cl/c.vue'
 export default {
+  components: { c },
   data() {
     return {
-      arRemID: [],
+      daRemUUID: [],
     }
   },
   mounted() {
     const resultSet = ormRem.query().get()
     if (resultSet.length) {
-      console.log('unsaved data found', resultSet, resultSet[0].uuid)
       for (let i = 0; i < resultSet.length; i++) {
-        this.arRemID.push(resultSet[i].uuid)
+        this.daRemUUID.push(resultSet[i].uuid)
       }
-    } else {
-      // When there is no unsaved data then we add an empty data to the state inside vuex
-      console.log('No Unsaved data')
     }
-  },
-  methods: {
-    getDescription(pRemID) {
-      console.log(pRemID)
-      const resultSet = ormRem.find(pRemID)
-      if (resultSet) {
-        console.log(resultSet)
-        // ['remDescription']
-        console.log(resultSet.uuid)
-        return resultSet.remDescription
-      } else {
-        return ''
-      }
-    },
-    setDescription(pEvent, pRemID) {
-      console.log('set called for', pRemID, pEvent)
-      const resultSet = ormRem.update({
-        where: pRemID,
-        data: {
-          remDescription: pEvent,
-        },
-      })
-      console.log(resultSet)
-    },
+    console.log(this.daRemUUID)
   },
 }
 </script>
