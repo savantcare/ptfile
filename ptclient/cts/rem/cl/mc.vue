8e8551e46d6fa40343a5228e8e248184fba5033f
Vikas <kedia.vikas@gmail.com>
2020-07-11 04:27:44 -0700

    caraousel for loop works

--- ptclient/cts/rem/cl/mc.vue
+++ ptclient/cts/rem/cl/mc.vue
@@ -1,10 +1,18 @@
+<!--
+The tree structure is:
+
+Carausel                           |
+  -- Carausel item                 |   These are the slides
+    -- Cards                       |   Each card is a reminder
+
+-->
 <template>
   <div class="block">
-    <el-carousel arrow="always" trigger="click">
-      <el-carousel-item v-for="slide in getNumOfCarouselItems">
+    <el-carousel arrow="always" trigger="click" @change="slideChanged">
+      <el-carousel-item v-for="slide in getNumOfCarouselSlides">
         <el-row type="flex">
-          <el-card v-for="set in getArrayOfReminderIDs">
-            <c :firstParam="set"></c>
+          <el-card v-for="set in getArrayOfRemIDs">
+            <changeRen :firstParam="set"></changeRen>
           </el-card>
         </el-row>
       </el-carousel-item>
@@ -13,20 +21,26 @@
 </template>
 <script>
 import ormRem from '@/cts/rem/vuex-orm/model.js'
-import c from '@/cts/rem/cl/c.vue'
+import changeRen from '@/cts/rem/cl/c.vue'
 export default {
-  components: { c },
+  components: { changeRen },
   data() {
     return {
       daUniqueIDOfEachRowFromORM: [],
+      diCurrentSlideNumber: 1,
     }
   },
   computed: {
-    getArrayOfReminderIDs() {
-      const arr = this.daUniqueIDOfEachRowFromORM.slice(1, 3)
+    getArrayOfRemIDs() {
+      const firstCard = this.diCurrentSlideNumber * 3
+      console.log('First Card', firstCard)
+      const arr = this.daUniqueIDOfEachRowFromORM.slice(
+        firstCard,
+        firstCard + 3
+      )
       return arr
     },
-    getNumOfCarouselItems() {
+    getNumOfCarouselSlides() {
       const count = this.daUniqueIDOfEachRowFromORM.length / 3
       const intValue = Math.floor(count)
       console.log('number of items in carousel are', count, intValue)
@@ -35,6 +49,12 @@ export default {
       } else {
         return intValue
       }
+    },
+  },
+  methods: {
+    slideChanged(val) {
+      console.log('slide changed', val)
+      this.diCurrentSlideNumber = val
     },
   },
   mounted() {
