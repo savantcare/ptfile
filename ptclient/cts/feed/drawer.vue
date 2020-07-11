d41c80e2d7c8b0cdd055678c045397b82c29bbf8
Vikas <kedia.vikas@gmail.com>
2020-07-11 15:02:33 -0700

    added framework for feed

---
+++ ptclient/cts/feed/drawer.vue
@@ -0,0 +1,21 @@
+<template>
+
+<el-drawer
+  title="I am the title"
+  :visible.sync="drawer"
+  :direction="ltr"
+  :show-close="false"
+  :with-header="false"
+  <span>Hi, there!</span>
+</el-drawer>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+}
+</script>
