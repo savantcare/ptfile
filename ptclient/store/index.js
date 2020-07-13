19cd5fdf5dd5126904e67017d28e628f252218b4
Vikas <kedia.vikas@gmail.com>
2020-07-13 01:59:08 -0700

    state maintained in model as a timeline so no need for flags

--- ptclient/store/index.js
+++ ptclient/store/index.js
@@ -6,7 +6,6 @@ import VuexormSearchUiToCT from '@vuex-orm/plugin-search'
 import VuexORM from '@vuex-orm/core'
 import axios from 'axios'
 import VuexORMAxios from '@vuex-orm/plugin-axios'
-import VuexORMisDirtyPlugin from '@vuex-orm/plugin-change-flags'
 
 // Ref: https://github.com/eldomagan/vuex-orm-localforage#installation
 import VueStateOfTabsAndDialogInCLModule from '@/cts/core/cl-tabs-manager/VueStateOfTabsAndDialogInCL'
@@ -23,7 +22,6 @@ import database from '@/database'
 const { v1: uuidv1 } = require('uuid')
 
 VuexORM.use(VuexORMAxios, { axios })
-VuexORM.use(VuexORMisDirtyPlugin)
 VuexORM.use(VuexormSearchUiToCT, {
   tokenize: true, // Ref: https://github.com/vuex-orm/plugin-search#configurations needed so "goal add" works when list has "add goal"
   matchAllTokens: true, // needed so "goal add" shows only opyion 1 when list has "add goal" and "multi rate goal"
