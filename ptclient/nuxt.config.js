fcf6f079e70df863ec87725e69022e5928398a7a
Vikas <kedia.vikas@gmail.com>
2020-07-18 18:55:16 -0700

    enabled nuxt to support typescript

--- ptclient/nuxt.config.js
+++ ptclient/nuxt.config.js
@@ -54,6 +54,7 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
+    '@nuxt/typescript-build',
   ],
   /*
    ** Nuxt.js modules
