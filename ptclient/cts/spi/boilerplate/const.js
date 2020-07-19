7ac365ca6ff447513dbd828754c433ea441b4b1f
Vikas <kedia.vikas@gmail.com>
2020-07-18 19:32:54 -0700

    more details to boilerp\late

---
+++ ptclient/cts/spi/boilerplate/const.js
@@ -0,0 +1,3 @@
+module.exports = {
+  BOILERPLATE_API_URL: 'http://localhost:8000/reminders',
+}
---
+++ ptclient/cts/spi/boilerplate/search-interfaces.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import ormSearchPhraseUiToCT from '@/cts/core/vl-search-box/vuex-orm/searchUIToCT'
+
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+    ormSearchPhraseUiToCT.insert({
+      data: {
+        value: 'boilper plate',
+        ctAbbr: 'bp',
+        ctToShowInMSVL: 'spi/rem/boilerplate/table.vue', // MSVL -> Multi state view layer
+        ctToShowInCSVL: 'spi/rem/boilerplate/timeline.vue', // CSVL -> Current state view layer
+        layer: 'view',
+      },
+    })
+  },
+}
+</script>
