2be923b48a8ea56398fe8059df0b52075aba5b25
Vikas <kedia.vikas@gmail.com>
2020-07-20 22:14:00 -0700

    added change in name ct

--- ptclient/cts/spi/name/vl/table.vue
+++ ptclient/cts/spi/name/vl/table.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Jai kali ma</div>
+  <div>
+    <el-button type="info" plain>Vikas</el-button>
+    <el-button type="info" plain>Kedia</el-button>
+    <el-button
+      type="primary"
+      size="mini"
+      style="padding: 3px;"
+      plain
+      @click="mfOpenCCtInCl(props.row.$id)"
+      >C</el-button
+    >
+  </div>
 </template>
 
 <script>
