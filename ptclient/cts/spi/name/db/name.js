16cc4eef5105d7693da6209991dfe88c0a638b65
Vikas <kedia.vikas@gmail.com>
2020-07-20 19:32:59 -0700

    added files for name

---
+++ ptclient/cts/spi/name/db/name.js
@@ -0,0 +1,33 @@
+// For docs read ptclient/docs/models.md
+import rowStatus from '@/cts/core/crud/rowstatus.js'
+
+const { v1: uuidv1 } = require('uuid')
+
+let count = 0
+const intUniqueID = () => ++count
+
+export default class reminders extends rowStatus {
+  static entity = 'rem'
+
+  static apiUrl = 'http://localhost:8000/reminders'
+
+  static fields() {
+    return {
+      ...super.fields(),
+
+      id: this.uid(() => intUniqueID()), // if this is not set then update based on primary key will not work
+      uuid: this.uid(() => uuidv1()),
+      ptUUID: this.string(null),
+      remDesc: this.string(''),
+      notes: this.string(null),
+      priority: this.number(0),
+      isAutoRem: this.number(0),
+      recordChangedByUUID: this.string(null),
+      recordChangedFromIPAddress: this.string(null),
+      recordChangedFromSection: this.string(null),
+
+      ROW_START: this.number(0),
+      ROW_END: this.number(2147483647.999999), // this is unix_timestamp value from mariaDB for ROW_END when a record is created new in MariaDB system versioned table.
+    }
+  }
+}
---
+++ ptclient/cts/spi/name/db/structure-gen-on-2020-06-26.sql
@@ -0,0 +1,16 @@
+use sc_rem;
+
+DROP TABLE IF EXISTS `rems`;
+
+CREATE TABLE `rems` (
+  `uuid` char(36) COLLATE utf8_unicode_ci NOT NULL,
+  `ptUUID` char(36) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `remDesc` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `notes` text DEFAULT NULL,
+  `priority` int(11) DEFAULT NULL,
+  `isAutoRem` int(11) DEFAULT NULL,
+  `recordChangedByUUID` char(36) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `recordChangedFromIPAddress` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `recordChangedFromSection` varchar(255) COLLATE utf8_unicode_ci DEFAULT 'patientFile',
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 WITH SYSTEM VERSIONING;
