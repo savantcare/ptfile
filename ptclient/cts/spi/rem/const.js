da55fe89e871c917d512c5feb41d51b1164c81bc
Vikas <kedia.vikas@gmail.com>
2020-07-23 14:28:50 -0700

    better doc

--- ptclient/cts/spi/rem/const.js
+++ ptclient/cts/spi/rem/const.js
@@ -1,3 +1,3 @@
 module.exports = {
-  REMINDER_API_URL: process.env.baseUrl + '/reminders',
+  REMINDER_API_URL: process.env.baseUrl + '/reminders', // By using process.env system can support dev prod and test env. baseurl is defined in nuxt.config.js
 }
