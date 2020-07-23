39dc273cf6136588398db3067cb4f56f84fdd6c5
Vikas <kedia.vikas@gmail.com>
2020-07-23 14:43:26 -0700

    now rem model comes from ct folder

--- ptclient/cts/spi/rem/files-for-ptserver.sh
+++ ptclient/cts/spi/rem/files-for-ptserver.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# This file is called by /ptfile/ptserver/copy-files-from-component-to-correct-locations.sh
 echo "Running file copy"
+
+echo "Copying: reminder.model.js"
+cp /gt/sc-prog-repos/ptfile/ptclient/cts/spi/rem/db/sequalize/reminder.model.js /gt/sc-prog-repos/ptfile/ptserver/models/
