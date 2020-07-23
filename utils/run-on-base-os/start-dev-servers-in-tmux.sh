68621dc524f66486b11fb2168bf3b1c2a4672da0
Vikas <kedia.vikas@gmail.com>
2020-07-22 19:54:25 -0700

    updated packages

--- utils/run-on-base-os/start-dev-servers-in-tmux.sh
+++ utils/run-on-base-os/start-dev-servers-in-tmux.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# If you have tmux but not tmuxinator:
-# Here panel exit when program ends
-# tmux new-session -s projectname 'yarn docs-in-dev-mode' \; split-window 'cd ptclient; yarn dev' \; split-window \; select-layout tiled \;
+# Scenario 1: If you do not have tmux
+# cd ptclient; yarn start-dev
+# cd ptserver; yarn start-dev
 
+# Scenario 2: If you have tmux but not tmuxinator:
+# With the following tmux command the panel will exit when program ends
+# tmux new-session -s projectname 'yarn docs-in-dev-mode' \; split-window 'cd ptclient; yarn start-dev' \; split-window \; select-layout tiled \;
+
+# Scenario 3: If you have tmuxinator. Used by core devs
 # Here panes do not exit when command ends
 tmuxinator start all-dev-servers-in-tmux
 
