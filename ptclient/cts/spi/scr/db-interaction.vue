d921876282481570b85422b2c041c58f9f1206a5
tanmoyjana-grmtech <tanmoyjana@grmtech.com>
2020-07-24 21:07:33 +0530

    added screening component left table

---
+++ ptclient/cts/spi/scr/db-interaction.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+<script>
+import { SCREENING_API_URL } from './const.js'
+import ormScr from '@/cts/spi/scr/db/vuex-orm/scr.js'
+export default {
+  data() {
+    return {}
+  },
+
+  async mounted() {
+    console.log('scr mounted function called')
+    // check length and only load if its empty
+    // Also need to restrict the load to current patient
+    const proScrFromDB = await ormScr.api().get(`${SCREENING_API_URL}/getAll`)
+    if (proScrFromDB.ok) {
+    }
+  },
+}
+</script>
