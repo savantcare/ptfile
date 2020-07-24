dec62f490ea423e04421ceaa66c0783290a57d25
Vikas <kedia.vikas@gmail.com>
2020-07-24 03:45:06 -0700

    trying to get build to work

--- utils/db/create-first-user.sh
+++ utils/db/create-first-user.sh
@@ -2,12 +2,8 @@
 
 
 # The following needs to be defined from github web interface of actions as a secret
-MYSQLPASS=jaidurgama 
 
-if [[ -z "$MYSQLPASS" ]]; then
-    echo "Must provide MYSQLPASS in environment" 1>&2
-    exit 1
-fi
+echo "Creating the user which ptclient will use to connect to ptserver"
 
 mysql -u root --password="qwerty" -e "CREATE USER 'stanford2008'@'%' IDENTIFIED BY '$MYSQLPASS';"
 mysql -u root --password="qwerty" -e "GRANT ALL PRIVILEGES ON *.* TO 'stanford2008'@'%';"
--- utils/db/restrict-root-user.sh
+++ utils/db/restrict-root-user.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo "Restricting root user from WAN connections"
+
 mysql -u root --password="qwerty" -e "DELETE FROM mysql.user WHERE Host = '%' AND User = 'root'"
