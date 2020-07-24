8e820c89d3c0aa449c0151e4a926029a385088e8
Vikas Kedia <kedia.vikas@gmail.com>
2020-07-24 23:47:15 +0530

    Update create-db-tables-mock-data-in-db.sh

--- utils/db/create-db-tables-mock-data-in-db.sh
+++ utils/db/create-db-tables-mock-data-in-db.sh
@@ -12,19 +12,19 @@ DB_USER="root"
 for file in $(find /gt/sc-prog-repos/ptfile/ptclient/cts/ -type f -name "create-db-*.sql" -print)
 do
     echo "== executing create DB $file";
-    mysql -u $DB_USER < $file 
+    mysql -u $DB_USER -pqwerty  < $file 
 done
 
 ## Step 2: structure of tables
 for file in $(find /gt/sc-prog-repos/ptfile/ptclient/cts/ -type f -name "structure-gen-*.sql" -print)
 do
     echo "== executing create DB $file";
-    mysql -u $DB_USER < $file 
+    mysql -u $DB_USER -pqwerty  < $file 
 done
 
 ## Mock data
 for file in $(find /gt/sc-prog-repos/ptfile/ptclient/cts/ -type f -name "mock-data*.sql" -print)
 do
     echo "== executing create DB $file";
-    mysql -u $DB_USER < $file 
+    mysql -u $DB_USER -pqwerty  < $file 
 done
