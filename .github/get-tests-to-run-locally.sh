c64b8f5761ae2bae088e3ef0cb0df250d3b54f6e
Vikas <kedia.vikas@gmail.com>
2020-07-25 00:29:29 -0700

    added the local runner

--- .github/get-tests-to-run-locally.sh
+++ .github/get-tests-to-run-locally.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-# Installing app from https://github.com/nektos/act
+echo "\n Step 1/6 Stopping all containers \n"
+docker kill $(docker ps -q)
+
+echo "\n Step 2/6 Removing all containers \n"
+docker rm $(docker ps -a -q)
+
+# remove all docker images
+# docker rmi $(docker images -q)
+
+echo "\n Step 3/6 Deleting the mysql data dir so previous run does not affect this run \n"
+rm -rf /tmp/var/
+
+echo "\n Step 4/6 Installing app from https://github.com/nektos/act \n"
 brew install nektos/tap/act
 
-# Download the full ubuntu image to make env similar to github https://github.com/nektos/act#alternative-runner-images
+echo "\n Step 5/6  Download the full ubuntu image to make env similar to github https://github.com/nektos/act#alternative-runner-images \n"
 docker run --rm -it --entrypoint "" nektos/act-environments-ubuntu:18.04-full ls
 
-# This will automatically pick up workflows/build.yml
+echo "\n Step 6/6 This will automatically pick up workflows/build.yml \n"
 act -P ubuntu-18.04=nektos/act-environments-ubuntu:18.04-full
