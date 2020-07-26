3773f8b8b67e8d335773fb4495ddfed1d5026376
Vikas <kedia.vikas@gmail.com>
2020-07-25 23:51:09 -0700

    getting supervisor to run mounted

--- ptclient/cts/spi/rem/const.js
+++ ptclient/cts/spi/rem/const.js
@@ -1,3 +1,4 @@
 module.exports = {
-  REMINDER_API_URL: 'locahost:8000/reminders', // By using process.env system can support dev prod and test env. baseurl is defined in nuxt.config.js
+  REMINDER_API_URL: 'http://localhost:8000/reminders',
+  // REMINDER_API_URL: process.env.baseUrl + '/reminders',  // By using process.env system can support dev prod and test env. baseurl is defined in nuxt.config.js
 }
