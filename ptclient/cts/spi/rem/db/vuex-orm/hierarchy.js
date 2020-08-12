b6781add6005e2e190e6fbacce41c6fdff889d78
Cue <1493221+cuebit@users.noreply.github.com>
2020-08-12 12:59:23 +0100

    chore(ptclient): exporting module explanation

--- ptclient/cts/spi/rem/db/vuex-orm/hierarchy.js
+++ ptclient/cts/spi/rem/db/vuex-orm/hierarchy.js
@@ -1,3 +1,5 @@
-export default rowStatus from '@/cts/core/crud/rowstatus.js'
+// export default rowStatus from '@/cts/core/crud/rowstatus.js'
+// export { default as rowStatus } from '@/cts/core/crud/rowstatus.js'
 
-export default rem from './rem.js'
+// export default rem from './rem.js'
+// export { default as rem } from './rem.js'
