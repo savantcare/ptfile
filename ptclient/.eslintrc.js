3a4354f3a9ff9fe22e434858e5e18261b882a8d2
Cue <1493221+cuebit@users.noreply.github.com>
2020-08-12 12:57:39 +0100

    fix(ptclient): eslint warning output

--- ptclient/.eslintrc.js
+++ ptclient/.eslintrc.js
@@ -18,11 +18,11 @@ module.exports = {
   // add custom rules here
   rules: {
     /*
-      Q) Why needed? 
+      Q) Why needed?
       - before every build eslint loader check all files and return format errors and warnings (eg:  no-console, whitspace ...) in console.
       - We do not want to show these errors in production environment as private data may leak.
     */
-    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'warning', // stop showing console warning from production enviroment
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'off' : 'warning', // stop showing debugger warning from production enviroment
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'warn', // stop showing console warning from production enviroment
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'off' : 'warn', // stop showing debugger warning from production enviroment
   },
 }
